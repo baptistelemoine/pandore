4e3c108dbcfc06ab99be44f2b71e20f7c8c2b571
Baptiste Lemoine <baptiste.lemoine@sigfox.com>
2020-03-08 08:42:37 +0100

    feat(layout): fix panel width

--- src/modules/main/index.js
+++ src/modules/main/index.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Box } from "@chakra-ui/core";
+import { Box, Flex } from "@chakra-ui/core";
 
 const MainLayout = ({ children }) => {
-  return <Box p={10}>{children}</Box>;
+  return (
+    <Flex justifyContent="center">
+      <Box p={10} w="100%" maxW="720px">
+        {children}
+      </Box>
+    </Flex>
+  );
 };
 
 export default MainLayout;
